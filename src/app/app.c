2e1880f4e981d17946b138b1bcb37240df325376
AlexsJones <jonesax@hush.com>
2015-03-21 16:19:11 +0000

    wip

--- src/app/app.c
+++ src/app/app.c
@@ -24,7 +24,7 @@
  #include <jnxc_headers/jnx_udp_socket.h>
 #include "app.h"
 #include "../gui/gui.h"
-#include <whisper-core/auth_comms.h>
+#include "auth_comms.h"
 
 #define END_LISTEN -1
 #define SESSION_INTERACTION "session_interaction"
